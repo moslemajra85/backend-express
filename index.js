a9b8c12a5236e354dc437521e4bd4f0b04f53f9f
moslemajra85 <144964665+moslemajra85@users.noreply.github.com>
2025-07-10 11:05:05 +0100

    control the fetch query

--- index.js
+++ index.js
@@ -3,52 +3,30 @@ const colors = require("colors");
 const express = require("express");
 const connectDB = require("./config/index");
 const Course = require("./models/course");
-const { difference } = require("lodash");
-
+const courses = require("./fake/data");
 const app = express();
 connectDB();
 
 async function createCourses() {
   try {
-    const result = await Course.insertMany([
-      {
-        name: "React - The Complete Guide",
-        author: "Adam Smith",
-        tags: ["Frontend", "JavaScript", "Library"],
-        isPublished: true,
-        price: 30.99,
-      },
-      {
-        name: "Node.js - The Complete Guide",
-        author: "Maximilian Schwarzmüller",
-        tags: ["Backend", "JavaScript", "Runtime"],
-        isPublished: true,
-        price: 15.0,
-      },
-      {
-        name: "Angular - The Complete Guide",
-        author: "Maximilian Schwarzmüller",
-        tags: ["Frontend", "JavaScript", "Framework"],
-        isPublished: false,
-        price: 20.0,
-      },
-      {
-        name: "MongoDB - The Complete Developer's Guide",
-        author: "Stephen Grider",
-        tags: ["Backend", "Database", "NoSQL"],
-        isPublished: true,
-        price: 10.0,
-      },
-    ]);
+    const result = await Course.insertMany(courses);
     console.log(result);
   } catch (error) {
     console.error(error.message);
   }
 }
 
+//createCourses();
 async function getCourses() {
   try {
-    // const courses = await Course.find();
+    const courses = await Course.find().limit(5).sort({
+      price: 1,
+    }).select({
+      name: 1,
+      author: 1,
+
+      _id: 0
+    });
 
     // const courses = await Course.find({
     //   tags: "Frontend",
@@ -58,19 +36,20 @@ async function getCourses() {
     //   _id: "686e3cc378551b4672470f7f",
     // });
 
-    const course = await Course.findById("686e3cc378551b4672470f7f");
+    // const course = await Course.findById("686e3cc378551b4672470f7f");
 
-    if (!course) {
-      console.log("Course was not found!".bgRed);
-      return;
-    }
+    // if (!course) {
+    //   console.log("Course was not found!".bgRed);
+    //   return;
+    // }
 
-    console.log(course);
+    console.log(courses);
   } catch (error) {
     console.error(error.message);
   }
 }
 
+getCourses();
 async function findCourses(criteria) {
   try {
     const courses = await Course.find(criteria);
@@ -149,12 +128,11 @@ async function updateCourse(id, updatedData) {
   }
 }
 
-updateCourse("686e4ba4d233c5a566844ba2", {
-  name: "C++",
-  author: "Moslem Ajra",
-  price: 20,
-});
-
+// updateCourse("686e4ba4d233c5a566844ba2", {
+//   name: "C++",
+//   author: "Moslem Ajra",
+//   price: 20,
+// });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
